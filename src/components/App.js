c6425fae9bd627ca9c6b21f859f3dee236871896
Eric Onyango <eric.onyango@student.moringaschool.com>
2023-01-19 22:18:10 +0300

    Assignment complete

--- src/components/App.js
+++ src/components/App.js
@@ -1,1 +1,24 @@
 // create your App component here
+import React, { useState, useEffect } from "react";
+
+function App() {
+  const [message, setMessage] = useState(null);
+
+  useEffect(() => {
+    fetch("https://dog.ceo/api/breeds/image/random")
+      .then((res) => res.json())
+      .then((data) => setMessage(data.message));
+  }, []);
+
+  if (message) {
+    return <><img src={message} alt="A Random Dog" /></>
+  } 
+    return (
+        <>
+            <p>Loading...</p>
+        </>
+        
+    );
+  }
+
+export default App;
